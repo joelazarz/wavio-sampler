cfe5b8622c5f6e0ad3ba271c3e14c9f1df9f97ba
joe lazarz <jlazarz87@gmail.com>
2020-10-25 17:19:33 -0400

    routes working

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
+const Kit = require('../models/Kit')
+const Region = require('../models/Region')
 
 // @route       POST  api/users
 // @desc        Register a user
@@ -63,15 +66,73 @@ async (req, res) => {
 // @route       PUT  api/users/:id
 // @desc        Edit a user
 // @access      private
-router.put('/:id', (req, res) => {
-  res.send('Edit a user');
+router.put('/:id',[auth, [
+  check('username', 'Username must be at least 3 characters').isLength({ min: 3 }),
+  check('email', 'Please include a valid email').isEmail()
+]], 
+async(req, res) => {
+  const validationErrors = validationResult(req);
+
+  if(!validationErrors.isEmpty()) {
+    return res.status(400).json({ errors: validationErrors.array() });
+  };
+
+  const { username, email } = req.body;
+  
+  // Build user object
+  const userFields = {};
+  if(username) userFields.username = username;
+  if(email) userFields.email = email;
+
+  try {
+    // Look for user
+    let user = await User.findById(req.params.id);
+
+    if(!user) return res.status(404).json({ msg: 'User not found' });
+
+    // Check to see if kit belongs to user
+    if(user._id.toString() !== req.user.id){
+      return res.status(401).json({ msg: 'Not authorized' });
+    };
+
+    user = await User.findByIdAndUpdate(req.params.id, 
+      { $set: userFields },   
+      { new: true }
+    );
+
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  };
 });
 
 // @route       DELETE  api/users/:id
 // @desc        Delete a user
 // @access      private
-router.delete('/:id', (req, res) => {
-  res.send('Delete a user');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    // Look for user
+    let user = await User.findById(req.params.id);
+
+    if(!user) return res.status(404).json({ msg: 'User not found' });
+
+    const userId = user._id.toString();
+
+    // Check to see if user belongs to user
+    if(userId !== req.user.id){
+      return res.status(401).json({ msg: 'Not authorized' });
+    };
+
+    await User.findByIdAndRemove(req.params.id);
+    await Kit.deleteMany({ user: userId });
+    await Region.deleteMany({ user: userId });
+
+    res.json({ msg: 'User removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  };
 });
 
 module.exports = router;
