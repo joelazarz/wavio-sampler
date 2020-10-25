1554abd8040276220dc6ecec84789bccf3e4adac
joe lazarz <jlazarz87@gmail.com>
2020-10-25 08:56:09 -0400

    get logged in user

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = function(req, res, next) {
 
   // check if token does not exist
   if(!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' })
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   };
 
   try {
