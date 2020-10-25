67c7dc39be004c53a93e79a4b48245618b514077
joe lazarz <jlazarz87@gmail.com>
2020-10-25 11:54:06 -0400

    create kit route

--- routes/kits.js
+++ routes/kits.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 const Kit = require('../models/Kit');
 
 // @route       POST  api/kits
 // @desc        Create kit
 // @access      private
-router.post('/', (req, res) => {
-  res.send('Create kit');
+router.post('/', [auth, [
+  check('name', 'Please add name').notEmpty(),
+  check('sample', 'Please upload sample').notEmpty()
+]], 
+async (req, res) => {
+  const validationErrors = validationResult(req);
 
-  kit = new Kit({
+  if(!validationErrors.isEmpty()) {
+    return res.status(400).json({ errors: validationErrors.array() });
+  };
 
-  });
+  const { name, sample } = req.body;
+
+  try {
+    const newKit = new Kit({
+    name,
+    sample,
+    user: req.user.id
+    });
+
+    const kit = await newKit.save();
+
+    res.json(kit);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  };
 });
 
 // @route       GET  api/kits
