85c58f3ddd5196bd42e28fb90ff79313727e74e3
joe lazarz <jlazarz87@gmail.com>
2020-10-29 20:00:08 -0400

    theme provider - client

---
+++ client/src/css/theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  nav: '#c4c9d4',
+  body: '#E2E2E2',
+  text: '#363537',
+}
+
+export const darkTheme = {
+  nav: '#313c46',
+  body: '#363537',
+  text: '#FAFAFA',
+}
