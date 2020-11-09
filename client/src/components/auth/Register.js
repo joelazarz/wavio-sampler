04b91eff98dc9d5660b2931528b06443237c27a0
joe lazarz <jlazarz87@gmail.com>
2020-11-09 12:00:47 -0500

    layout setup

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,20 @@
-import { AuthForm } from '../../css/authForm';
+import { AuthForm,
+  AuthFormInput, 
+  AuthFormLabel, 
+  AuthFormButton, 
+  AuthFormTitle } from '../../css/authForm';
 import Preview from './Preview';
 
 const Register = () => {
   return (
     <>
     <AuthForm>
-      Register Component
+      <AuthFormTitle>Register</AuthFormTitle>
+      <AuthFormLabel htmlFor='email'>Email Address</AuthFormLabel>
+      <AuthFormInput type="email" required></AuthFormInput>
+      <AuthFormLabel htmlFor='email'>Password</AuthFormLabel>
+      <AuthFormInput type="password" required></AuthFormInput>
+      <AuthFormButton type="submit" value='Register'></AuthFormButton>
     </AuthForm>
     <Preview />
     </>
--- client/src/css/authForm.js
+++ client/src/css/authForm.js
@@ -5,10 +5,45 @@ grid-row-start: 2;
 grid-row-end: 6;
 /* height: 30vh; */
 grid-column-start: 2;
-grid-column-end: 5;
-margin: 1rem;
+grid-column-end: 8;
+margin: 1rem 1rem;
 display: flex;
-justify-content: center;
+flex-direction: column;
+/* justify-content: center; */
+/* place-items: center; */
 background-color: rgba(255, 255, 255, 0.2);
 border-radius: 0.25rem;
+`
+
+export const AuthFormInput = styled.input`
+width: 90%;
+border: none;
+border-radius: 0.25em;
+padding: 0.5rem;
+background-color: rgba(255, 255, 255, 0.8);
+box-shadow: none;
+font-family: 'Roboto Mono', monospace;
+margin: 0.5rem auto;
+`
+
+export const AuthFormButton = styled.input`
+width: 50%;
+font-family: 'Roboto Mono', monospace;
+font-size: 13px;
+margin: 0.5rem auto 1rem auto;
+border: none;
+border-radius: 0.25em;
+background-color: rgba(255, 255, 255, 0.6);
+`
+
+export const AuthFormLabel = styled.label`
+margin: 0.2rem auto 0rem 2rem;
+`
+
+export const AuthFormTitle = styled.div`
+background-color: rgba(255, 255, 255, 0.2);
+border-radius: 0.25em;
+padding: 0 4rem;
+margin: 0.5rem auto;
+font-size: 15px;
 `
